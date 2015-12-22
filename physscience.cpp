5f5a4f68ecbe64d83ba1a9abc07dc3e697d06bc7
caolan <caolan@aeshna.net>
2015-12-21 23:43:13 -0800

    misspelling in epsilon

--- physscience.cpp
+++ physscience.cpp
@@ -3,5 +3,5 @@
 
 
 double coulomb(const double q1, const double q2, const double r) {
-    return (1 / (4 * PhysConsts::PI * PhysConsts::episilon_zero)) * ((abs(q1) * abs(q2)) / r * r);
+    return (1 / (4 * PhysConsts::PI * PhysConsts::epsilon_zero)) * ((abs(q1) * abs(q2)) / r * r);
 }
--- physscience.h
+++ physscience.h
@@ -4,7 +4,7 @@
 namespace PhysConsts {
     static const double PI = 3.14159265358979323846264338327950288419717;
     static const double DefaultMass = 1.0;  // 1kg
-    static const double episilon_zero = 8.85e-12;   // permittivity constant (electromagnetics)
+    static const double epsilon_zero = 8.85e-12;   // permittivity constant (electromagnetics)
     static const double e = 1.60e-19;       // Measured in Coulombs
     static const double K = 8.99e9;         // Electrostatic Konstant
 
