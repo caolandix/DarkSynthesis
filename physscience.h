c8f19e27688c7ee434ae92592e703dd0aaf67f0d
Caolan O'Domhnaill <caolandix@users.noreply.github.com>
2026-04-07 09:22:00 -0700

    Update physscience.h

--- physscience.h
+++ physscience.h
@@ -1,11 +1,14 @@
 #ifndef PHYSSCIENCE_H
 #define PHYSSCIENCE_H
 
+#include <numbers>
+using namespace std;
+
 namespace PhysConsts {
-    static const double PI =            3.14159265358979323846264338327950288419717;
+    static const double PI =            std::numbers::pi;
     static const double DefaultMass =   1.0;                                        // 1kg
     static const double epsilon_zero =  8.85e-12;                                   // permittivity constant (electromagnetics)
-    static const double e =             1.60e-19;                                   // Measured in Coulombs
+    static const double e =             std::numbers::e;                                   // Measured in Coulombs
     static const double K =             8.99e9;                                     // Electrostatic Konstant
     static const double earth_g =       9.8;                                        // force of gravity constant on earth
 
